2f2bfe824bd4c7c11424e5fa2e64c6108dbf439d
KirtanPatel8 <kirtansantoki12@gmail.com>
2024-10-09 16:09:09 +0530

    Initial Commit

--- app/src/main/java/com/devkt/mad_practical_6_22012011045/MainActivity.kt
+++ app/src/main/java/com/devkt/mad_practical_6_22012011045/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.devkt.mad_practical_6_22012011045
 
+import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
+import android.view.animation.Animation
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() , Animation.AnimationListener{
+    lateinit var alarmAnimation: AnimationDrawable
+    lateinit var heartAnimation: AnimationDrawable
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +21,31 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if(hasFocus){
+            alarmAnimation = findViewById<ImageView>(R.id.alarm_img).background as AnimationDrawable
+            heartAnimation = findViewById<ImageView>(R.id.heart_anim).background as AnimationDrawable
+            alarmAnimation.start()
+            heartAnimation.start()
+        }
+        else{
+            alarmAnimation.stop()
+            heartAnimation.stop()
+        }
+    }
+
+    override fun onAnimationStart(animation: Animation?) {
+
+    }
+
+    override fun onAnimationEnd(animation: Animation?) {
+
+    }
+
+    override fun onAnimationRepeat(animation: Animation?) {
+
     }
 }
